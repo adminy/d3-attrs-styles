1aaa5ed938b07947cf78cb1929d137e507004aba
Adminy <adminy@users.noreply.github.com>
2023-02-24 19:23:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-import { select, selection as d3selection } from 'd3-selection';
+import { select, selection, transition } from 'd3-selection'
 
 const getFunction = type => function( data ) {
-  if( typeof data === 'function' ) {
-    return this.each( function() {
-      let cur = select( this ),
-          obj = data.apply( this, arguments );
-    
-      for ( let attrName in obj ) cur[ type ]( attrName, obj[attrName] );
-    });
+  if (typeof data === 'function') {
+    return this.each(function() {
+      const cur = d3.select( this )
+      const obj = data.apply( this, arguments )
+      for ( let attrName in obj )
+        cur[ type ]( attrName, obj[attrName] )
+    })
   }
-
   for( let attrName in data )
-    this[ type ]( attrName, data[attrName] );
-
-  return this;
+    this[ type ]( attrName, data[attrName] )
+  return this
 }
 
-d3selection.prototype.attrs = getFunction( 'attr' );
-d3selection.prototype.styles = getFunction( 'style' );
+selection.prototype.attrs = transition.prototype.attrs = getFunction( 'attr' )
+selection.prototype.styles = transition.prototype.styles = getFunction( 'style' )
