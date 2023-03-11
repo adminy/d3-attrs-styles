4ee582c687923267815a49d53ac623f1fef8c57d
adminy <marinbivol10@gmail.com>
2023-03-11 16:25:56 +0000

    simplify

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-import { select, selection } from 'd3-selection'
+import { selection } from 'd3-selection'
 import { transition } from 'd3-transition'
 
-const getFunction = type => function( data ) {
-  if (typeof data === 'function') {
-    return this.each(function() {
-      const cur = select( this )
-      const obj = data.apply( this, arguments )
-      for ( let attrName in obj )
-        cur[ type ]( attrName, obj[attrName] )
-    })
-  }
-  for( let attrName in data )
-    this[ type ]( attrName, data[attrName] )
-  return this
-}
-
+const getFunction = type => function( data ) { Object.entries(data).map(([key, val]) => this[type](key, val)); return this }
 selection.prototype.attrs = transition.prototype.attrs = getFunction( 'attr' )
 selection.prototype.styles = transition.prototype.styles = getFunction( 'style' )
